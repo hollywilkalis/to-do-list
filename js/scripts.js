d331f590543405c2ac725930cc8a279fdabcc782
Cat Janowitz and Holly Wilkalis <cat@>
2017-10-30 16:42:14 -0700

    Add functionality to hide completed items q

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
 //business logic:
-function Task(theTask){
+function Task(theTask, dueDate) {
     this.toDo = theTask;
+    this.dueDate = dueDate;
 }
 
+Task.prototype.allTaskInfo = function() {
+  return this.toDo + ", due" + " " + this.dueDate;
+}
+
+
+//user interfact logic:
 $(document).ready(function() {
   $("form#list").submit(function(event) {
     event.preventDefault();
-
     var inputtedTask = $("input#newTask").val();
+    var inputtedDueDate = $("input#dueDate").val();
 
-    var getDone = new Task(inputtedTask);
+    var getDone = new Task(inputtedTask, inputtedDueDate);
 
-    $("ul#taskList").append("<li><span class='taskItem'>" + getDone.toDo + "</span></li>");
+    $("ul#taskList").append("<li><span class='taskItem'>" + getDone.allTaskInfo() + "</span></li>");
 
-    // $("ul#taskList").click()
+    $("ul#taskList").on("click", "li", function() {
+      $(this).remove();
+    });
 
     $("input#newTask").val("");
+    $("input#dueDate").val("");
 
   });
 });
